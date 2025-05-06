d4279d10ce721bc2a07204b65752c98e7eef9edb
Mithlesh Kumar Gupta <84225437+mithleshgupta@users.noreply.github.com>
2025-05-06 11:34:37 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 import { io } from "socket.io-client";
 import "./App.css";
 
-const socket = io("http://localhost:3001");
+const socket = io("https://tradebackend-4.onrender.com");
 
 function App() {
   const [marketData, setMarketData] = useState([]);
